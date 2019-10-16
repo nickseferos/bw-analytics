72754e93fd15a19123c0344ea8b260e808bd0ffc
nickseferos <nick.seferos@gmail.com>
2019-10-16 16:43:22 -0700

    Install theme and styleize.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @user_port_calls = PortCall.where(terminal_id: current_user.terminal_id)
+    @user_upcoming_port_calls = PortCall.where(terminal_id: current_user.terminal_id)
   end
 end
