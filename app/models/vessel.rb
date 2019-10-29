691bc498a4931364c5ac966669690fd622b6f851
nickseferos <nick.seferos@gmail.com>
2019-10-28 21:00:49 -0700

    modify user control views

--- app/models/vessel.rb
+++ app/models/vessel.rb
@@ -1,3 +1,7 @@
 class Vessel < ApplicationRecord
   belongs_to :steamshipline
+
+  def to_param
+    "#{id} #{name}".parameterize
+  end
 end
--- app/models/voyage.rb
+++ app/models/voyage.rb
@@ -2,5 +2,7 @@ class Voyage < ApplicationRecord
   belongs_to :vessel
   has_many :port_calls, dependent: :destroy
 
-
+  def to_param
+    "#{id} #{number}".parameterize
+  end
 end
